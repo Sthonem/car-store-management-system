6f484e0dcd98c1629972ae68090e29ba472c96de
Piotr <1234@123>
2026-03-13 14:25:31 +0100

    car places

--- main.py
+++ main.py
@@ -7,5 +7,15 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-if __name__ == "__main__":
+@app.get("/carPlaces")
+async def get_car_places():
+    return [
+        {"id": 0, "name": "Ford", "color": "red", "place": "Gdańsk"},
+        {"id": 1, "name": "BMW", "color": "blue", "place": "Łódź"},
+        {"id": 2, "name": "Mercedes", "color": "green", "place": "Gdynia"},
+        {"id": 3, "name": "Renault", "color": "silver", "place": "Sopot"},
+        {"id": 4, "name": "Tesla", "color": "white", "place": "Sosnowiec"},
+    ]
+
+if __name__ == "_main_":
     uvicorn.run(app, host="0.0.0.0", port=8000)
