d6e101ca77d672612e336dd6ff8ab91699722294
micz5596 <miczjanta@gmail.com>
2026-03-13 14:28:32 +0100

    Owners

--- main.py
+++ main.py
@@ -7,5 +7,18 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/car_owners")
+async def get_car_owners():
+    return [
+        {"id": 1, "name": "Ford", "owner" : "Adam"},
+        {"id": 2, "name": "BMW", "owner": "Pawel"},
+        {"id": 3, "name": "Mercedes", "owner": "Ken"},
+        {"id": 4, "name": "Renault", "owner": "Will"},
+        {"id": 5, "name": "Tesla", "owner": "Steve"}
+    ]
+
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
